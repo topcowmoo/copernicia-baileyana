af32483c62267f7bdad55a67b5a873f125683bc8
Salvatore <mammoliti.sam@gmail.com>
2024-04-27 20:44:48 -0400

    refactor

--- server/db/connection.js
+++ server/db/connection.js
@@ -12,7 +12,7 @@ const client = new MongoClient(uri, {
 try {
   await client.connect();
   await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connted to MongoDB!");
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
 } catch (error) {
   console.error(error);
 }
